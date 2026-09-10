15fea23d90a3e9d95c6b397f370e7cfc28ba0b32
MNIK0211 <jayarajniketh4@gmail.com>
2026-09-10 15:51:58 +0530

    update todo listwith forEach

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -11,46 +11,20 @@ renderTodoList();
 function renderTodoList(){
 let todoListHTML = '';
 
-for(let i = 0; i<todoList.length; i++){
-  const todoobject = todoList[i];
-  //const name = todoobject.name;
-  //const dueDate = todoobject.dueDate;
+todoList.forEach(function(todoobject,index){
   const {name, dueDate} = todoobject;
   const html = `
   <div>${name}</div>
   <div> ${dueDate}</div>
     <button onclick="
-    todoList.splice(${i}, 1);
+    todoList.splice(${index}, 1);
     renderTodoList();
   "class = "delete-todo-button">Delete</button>
 
 `;
 todoListHTML += html;
-}
 
-
-document.querySelector('.js-todo-list')
-.innerHTML = todoListHTML;
-}
-
-
-
-function addTodo(){
-  const inputElement= document.querySelector('.js-name-input');
-  const name  = inputElement.value;
-
-const dateInputElement = document.querySelector('.js-due-date-input');
-const dueDate = dateInputElement.value;
-
-  todoList.push({
-    name,
-    dueDate
-  });
-
-
-  inputElement.value='';
-
-  renderTodoList();
+});
 }
 
 
